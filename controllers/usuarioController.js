2d2565895696526070ab0990ff488cdbe5f684a3
AlbeTorres <albertocorreoficial@gmail.com>
2022-06-02 19:35:30 -0400

    añadido token

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,6 +1,15 @@
 const Usuario = require('../models/Usuario');
+const bcryptjs = require('bcryptjs');
+const { validationResult} = require('express-validator');
+const jwt = require('jsonwebtoken');
 
 exports.crearUsuario= async(req,res)=>{
+
+    //revisar si hay errores
+    const errores = validationResult(req);
+    if(!errores.isEmpty()){
+        return res.status(400).json({errores: errores.array()});
+    }
 
     const {email, password} = req.body
     
@@ -16,11 +25,33 @@ exports.crearUsuario= async(req,res)=>{
 
         usuario =new Usuario(req.body);
 
+        //hashear el password
+        const salt = await bcryptjs.genSalt(10);
+        usuario.password = await bcryptjs.hash(password, salt);
+
+
         //guardar usuario
         await usuario.save();
 
-        //mensaje de respuesta good
-        res.status(200).json({msg:'Usuario creado correctamente'});
+        //crear y firmar jwt
+        const payload ={
+            usuario: {
+                id: usuario.id
+            }
+        };
+
+        //firmar el jwt
+        jwt.sign(payload, process.env.SECRETA, {
+            expiresIn: 3600000
+
+        },(error, token)=>{
+            if(error) throw error;
+
+            //Mensaje de confirmacion
+            res.status(200).json({token});
+
+
+        });
 
     } catch (error) {
         console.log(error);
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -3,10 +3,17 @@
 const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
+const {check} = require('express-validator');
 
 //Crea un usuario
 //api/usuarios
 router.post('/',
+    [
+        check('nombre', 'El nombre es obligatorio ').not().isEmpty(),
+        check('email','Agrega un email válido').isEmail(),
+        check('password', 'El password mínimo es de 6 caracteres').isLength({min:6})
+
+    ],
     usuarioController.crearUsuario
 );
 
