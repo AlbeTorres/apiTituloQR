ccccc3e1967965658f60a84612ed57ce8c7e9a5c
AlbeTorres <albertocorreoficial@gmail.com>
2022-06-02 11:29:38 -0400

    añadido crear usuario db

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,3 +1,26 @@
-exports.crearUsuario=(req,res)=>{
-    console.log('desde crear usuario')
+const Usuario = require('../models/Usuario');
+
+exports.crearUsuario= async(req,res)=>{
+    
+    try{
+
+        let usuario;
+
+        //crea el usuario
+
+        usuario =new Usuario(req.body);
+
+        //guardar usuario
+        await usuario.save();
+
+        //mensaje de respuesta good
+        res.send('Usuario creado correctamente');
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('Hubo un Error')
+    }
+
+    
+    
 }
--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express();
 //conectar la base de datos
 conectarDB();
 
+//Habilitar Express.json
+app.use(express.json({ extended: true }));
+
 // puerto de la app
 const PORT = process.env.PORT || 4000;
 
@@ -17,4 +20,4 @@ app.use('/api/usuarios', require('./routes/usuarios'))
 // arrancar la app
 app.listen(PORT, ()=>{
     console.log(`El servidor está corriendo en el puerto ${PORT}`)
-});
+}); 
